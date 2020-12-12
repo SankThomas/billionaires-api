803d89a6fae22494b72fc0ed29048a16e522a580
SankThomas <thomassankara74@gmail.com>
2020-12-12 17:13:58 +0300

    billionaires api - first commit

---
+++ dist/data.js
@@ -0,0 +1,303 @@
+// source: https://forbes.com/billionaires
+export const billionaires = [
+  {
+    id: 1,
+    netWorth: '113B',
+    name: 'Jeff Bezos',
+    country: 'United States',
+    source: 'Amazon',
+    industry: 'Technology',
+    image: '',
+    age: 56
+  },
+  {
+    id: 2,
+    netWorth: '98B',
+    name: 'Bill Gates',
+    country: 'United States',
+    source: 'Microsoft',
+    industry: 'Technology',
+    image: '',
+    age: 64
+  },
+  {
+    id: 3,
+    netWorth: '76B',
+    name: 'Bernard Arnault & Family',
+    country: 'France',
+    source: 'LVMH',
+    industry: 'Fashion & Retail',
+    image: '',
+    age: 71
+  },
+  {
+    id: 4,
+    netWorth: '67.5B',
+    name: 'Warren Buffet',
+    country: 'United States',
+    source: 'Berkeshire Hathaway',
+    industry: 'Finance & Investments',
+    image: '',
+    age: 89
+  },
+  {
+    id: 5,
+    netWorth: '59B',
+    name: 'Larry Ellison',
+    country: 'United States',
+    source: 'software',
+    industry: 'Technology',
+    image: '',
+    age: 75
+  },
+  {
+    id: 6,
+    netWorth: '55.1B',
+    name: 'Amancio Ortega',
+    country: 'Spain',
+    source: 'Zara',
+    industry: 'Fashion & Retail',
+    image: '',
+    age: 84
+  },
+  {
+    id: 7,
+    netWorth: '54.7B',
+    name: 'Mark Zuckerberg',
+    country: 'United States',
+    source: 'Facebook',
+    industry: 'Technology',
+    image: '',
+    age: 35
+  },
+  {
+    id: 8,
+    netWorth: '54.6B',
+    name: 'Jim Walton',
+    country: 'United States',
+    source: 'Walmart',
+    industry: 'Fashion & Retail',
+    image: '',
+    age: 71
+  },
+  {
+    id: 9,
+    netWorth: '54.4B',
+    name: 'Alice Walton',
+    country: 'United States',
+    source: 'Walmart',
+    industry: 'Fashion & Retail',
+    image: '',
+    age: 70
+  },
+  {
+    id: 10,
+    netWorth: '54.1B',
+    name: 'Rob Walton',
+    country: 'United States',
+    source: 'Walmart',
+    industry: 'Fashion & Retail',
+    image: '',
+    age: 75
+  },
+  {
+    id: 11,
+    netWorth: '52.7B',
+    name: 'Steve Ballmer',
+    country: 'United States',
+    source: 'Microsoft',
+    industry: 'Technology',
+    image: '',
+    age: 64
+  },
+  {
+    id: 12,
+    netWorth: '52.1B',
+    name: 'Carlos Slim Helu & Family',
+    country: 'Mexico',
+    source: 'telecom',
+    industry: 'Telecom',
+    image: '',
+    age: 80
+  },
+  {
+    id: 13,
+    netWorth: '50.9B',
+    name: 'Larry Page',
+    country: 'United States',
+    source: 'Google',
+    industry: 'Technology',
+    image: '',
+    age: 47
+  },
+  {
+    id: 14,
+    netWorth: '49.1B',
+    name: 'Sergey Brin',
+    country: 'United States',
+    source: 'Google',
+    industry: 'Technology',
+    image: '',
+    age: 46
+  },
+  {
+    id: 15,
+    netWorth: '48.9B',
+    name: 'Francoise Betterncourt Meyers & Family',
+    country: 'France',
+    source: "L'Oreal",
+    industry: 'Fashion & Retail',
+    image: '',
+    age: 66
+  },
+  {
+    id: 16,
+    netWorth: '48B',
+    name: 'Michael Bloomberg',
+    country: 'United States',
+    source: 'Bloomberg LP',
+    industry: 'Meddia & Entertainment',
+    image: '',
+    age: 78
+  },
+  {
+    id: 17,
+    netWorth: '38.8B',
+    name: 'Jack Ma',
+    country: 'China',
+    source: 'Alibaba',
+    industry: 'Tecnology',
+    image: '',
+    age: 55
+  },
+  {
+    id: 18,
+    netWorth: '38.2B',
+    name: 'Charles Koch',
+    country: 'United States',
+    source: 'Koch Industries',
+    industry: 'Diversified',
+    image: '',
+    age: 84
+  },
+  {
+    id: 19,
+    netWorth: '38.2B',
+    name: 'Julia Koch & Family',
+    country: 'United States',
+    source: 'Koch Industries',
+    industry: 'Diversified',
+    image: '',
+    age: 57
+  },
+  {
+    id: 20,
+    netWorth: '38.1B',
+    name: 'Ma Huateng',
+    country: 'China',
+    source: 'internet media',
+    industry: 'Technology',
+    image: '',
+    age: 48
+  },
+  {
+    id: 21,
+    netWorth: '36.8B',
+    name: 'Mukesh Ambani',
+    country: 'India',
+    source: 'petrochemicals, oil & gas',
+    industry: 'Energy',
+    image: '',
+    age: 62
+  },
+  {
+    id: 22,
+    netWorth: '36B',
+    name: 'Mackenzie Scott',
+    country: 'United States',
+    source: 'Amazon.com',
+    industry: 'Technology',
+    image: '',
+    age: 50
+  },
+  {
+    id: 23,
+    netWorth: '33.3B',
+    name: 'Beate Heister & Karl Albrecht Jr.',
+    country: 'Germany',
+    source: 'supermarkets',
+    industry: 'Fashion & Retail',
+    image: '',
+    age: 69
+  },
+  {
+    id: 24,
+    netWorth: '31.6B',
+    name: 'David Thomson & Family',
+    country: 'Canada',
+    source: 'media',
+    industry: 'Media & Entertainment',
+    image: '',
+    age: 62
+  },
+  {
+    id: 25,
+    netWorth: '29.5B',
+    name: 'Phil Knight & Family',
+    country: 'United States',
+    source: 'Nike',
+    industry: 'Fashion & Retail',
+    image: '',
+    age: 82
+  },
+  {
+    id: 26,
+    netWorth: '28.1B',
+    name: 'Lee Shau Kee',
+    country: 'Honk Kong',
+    source: 'real estate',
+    industry: 'Real Estate',
+    image: '',
+    age: 92
+  },
+  {
+    id: 27,
+    netWorth: '27B',
+    name: 'Francois Pinault & Family',
+    country: 'France',
+    source: 'luxury goods',
+    industry: 'Fashion & Retail',
+    image: '',
+    age: 83
+  },
+  {
+    id: 28,
+    netWorth: '26.8B',
+    name: 'Sheldon Adelson',
+    country: 'United States',
+    source: 'casinos',
+    industry: 'Gambling & Casinos',
+    image: '',
+    age: 86
+  },
+  {
+    id: 29,
+    netWorth: '24.7B',
+    name: 'Jacqeuline Mars',
+    country: 'United States',
+    source: 'candy, pet food',
+    industry: 'Food & Beverage',
+    image: '',
+    age: 80
+  },
+  {
+    id: 30,
+    netWorth: '24.7B',
+    name: 'John Mars',
+    country: 'United States',
+    source: 'candy, pet food',
+    industry: 'Food & Beverage',
+    image: '',
+    age: 84
+  }
+]
